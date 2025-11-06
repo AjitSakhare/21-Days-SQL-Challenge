7caebcaaeba898ca398c6268b5fcfed65436cc05
Ajit Sakhare <45509860+AjitSakhare@users.noreply.github.com>
2025-11-06 18:35:11 +0530

    Day 1 - Introduction to SQL & SELECT Statement

---
+++ Day 1- Introduction to SQL & SELECT Statement.sql
@@ -0,0 +1,9 @@
+### Day 1 (03/11): Introduction to SQL & SELECT Statement
+
+**Topics:** Basic SELECT, column selection, viewing data structure
+
+Challenge - List all unique hospital services available in the hospital.
+
+
+		select distinct service from dbo.services_weekly
+		
